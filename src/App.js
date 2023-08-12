669307c90fb70859e58b8bdfdb67f180f3755655
Anish0369 <anish231003@gmail.com>
2023-08-12 23:28:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import CssBaseline from '@mui/material/CssBaseline';
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1> Hello World </h1>
+      <p>Hey This is Anish . VSA</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div
+    className='flex flex-col items-center justify-center h-screen text-4xl bg-gray-200'
+    
+    >home</div>
+  )
+}
